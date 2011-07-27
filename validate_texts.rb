5dc87230681af30b631af0557d8ced0a467b45b5
Dirk Jost <dirk@railslove.com>
2011-07-27 15:52:37 +0200

    fix texts schema and test

--- validate_texts.rb
+++ validate_texts.rb
@@ -13,7 +13,7 @@ describe 'texts' do
       begin
         json_str = File.open(file) { |f| f.read }
         # force_encoding only available in ruby 1.9.2
-        job = JSON.parse( json_str.respond_to?(:force_encoding ) ? json_str.force_encoding('UTF-8') : json_str)['texts']
+        job = JSON.parse( json_str.respond_to?(:force_encoding ) ? json_str.force_encoding('UTF-8') : json_str)['text']
         JSON::Validator.validate!(schema, job, :version=> :draft3 )
       rescue JSON::Schema::ValidationError => schema_error
          assertion = false, schema_error.message + "\nIn: #{file}\nplease fix this!"
